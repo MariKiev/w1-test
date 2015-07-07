3474511a9831cec2a2d0921bd30912e496c4198d
Maryna Beskorovainaya <iam.marik@gmail.com>
2015-07-07 20:03:47 +0300

    change URL in client.py

--- client.py
+++ client.py
@@ -2,11 +2,11 @@ import requests
 import json
 
 def get_user(id):
-    response = requests.get('http://localhost:5000/api/users/%s' % id)
+    response = requests.get('https://w1-test.herokuapp.com/api/users/%s' % id)
     return json.loads(response.text)
 
 def create_user(username, email, phone_number, pets):
-    response = requests.post('http://localhost:5000/api/users', data={'username': username, 
+    response = requests.post('https://w1-test.herokuapp.com/api/users', data={'username': username, 
         'email': email,
         'phone_number': phone_number,
         'pets': pets
